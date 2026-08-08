0b54dc07cd89a5cfd17995b60bbb2e67214f9df9
T. Franzel <tfranzel@users.noreply.github.com>
2026-08-08 12:55:46 +0200

    add CLI start options

--- kirk/run.py
+++ kirk/run.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import importlib
 import os
@@ -9,32 +10,72 @@ from kirk.kirk import Kirk
 from kirk.transporter import Transporter
 
 
-async def main() -> None:
-    """Initialize and run Kirk IRC client from configuration."""
-    with open(os.path.expanduser("~/.kirk.toml"), "rb") as fh:
-        config = tomllib.load(fh)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="kirk",
+        description="Kirk - a minimalistic IRC client and curses UI.",
+    )
+    parser.add_argument(
+        "--host",
+        help="IRC server host to connect to, e.g. irc.libera.chat. "
+        "If given together with --nick, ~/.kirk.toml is ignored.",
+    )
+    parser.add_argument("--nick", help="Nickname to use on the server.")
+    parser.add_argument(
+        "--no-ssl", dest="ssl", action="store_false", default=True, help="Connect without SSL/TLS."
+    )
+    parser.add_argument(
+        "--log", action="store_true", help="Log session activity to file instead of discarding it."
+    )
+    parser.add_argument(
+        "--config",
+        default="~/.kirk.toml",
+        help="Path to the TOML config file (default: ~/.kirk.toml). Ignored if --host and --nick are given.",
+    )
+    return parser.parse_args()
 
-    if "client_class" in config["kirk"]:
-        *path, class_name = config["kirk"]["client_class"].split(".")
-        client_class: type[IrcClient] = getattr(importlib.import_module(".".join(path), "."), class_name)
+
+async def main(args: argparse.Namespace) -> None:
+    """Initialize and run Kirk IRC client from CLI arguments or a configuration file."""
+    persistence = False
+
+    if args.host and args.nick:
+        clients = [
+            IrcClient(
+                host=args.host,
+                nick=args.nick,
+                ssl=args.ssl,
+                log_mode="file" if args.log else "none",
+            )
+        ]
     else:
-        client_class = IrcClient
-    persistence = config["kirk"].get("persistence", False)
+        with open(os.path.expanduser(args.config), "rb") as fh:
+            config = tomllib.load(fh)
+
+        if "client_class" in config["kirk"]:
+            *path, class_name = config["kirk"]["client_class"].split(".")
+            client_class: type[IrcClient] = getattr(
+                importlib.import_module(".".join(path), "."), class_name
+            )
+        else:
+            client_class = IrcClient
+        persistence = config["kirk"].get("persistence", False)
+
+        clients = [
+            client_class(
+                host=c["host"],
+                nick=c["nick"],
+                auth=c.get("auth", None),
+                auto_join=c.get("auto_join", None),
+                keys=c.get("keys", {}),
+                dcc_dir=os.path.expanduser(c.get("dcc_dir", "~/Downloads/")),
+                ssl=c.get("ssl", True),
+                log_mode=c.get("log_mode", "none"),
+            )
+            for c in config["kirk"]["client"]
+        ]
 
     loop = asyncio.get_running_loop()
-    clients = [
-        client_class(
-            host=c["host"],
-            nick=c["nick"],
-            auth=c.get("auth", None),
-            auto_join=c.get("auto_join", None),
-            keys=c.get("keys", {}),
-            dcc_dir=os.path.expanduser(c.get("dcc_dir", "~/Downloads/")),
-            ssl=c.get("ssl", True),
-            log_mode=c.get("log_mode", "none"),
-        )
-        for c in config["kirk"]["client"]
-    ]
     kirk = Kirk(clients, loop)
 
     # blessed's notify_on_resize() relies on the terminal supporting in-band
@@ -59,4 +100,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main(), debug=False)
+    asyncio.run(main(parse_args()), debug=False)
